b0b6b7f90a6a84cf0ebd460b272b006a165ec84c
alfonso zbook <zbook@reyesaguayo.com>
2017-05-01 12:12:20 -0500

    Euler combined

--- R/Euler.R
+++ R/Euler.R
@@ -22,11 +22,19 @@ setMethod("initialize", "Euler", function(.Object, ode, ...) {
 
 setMethod("init", "Euler", function(object, stepSize, ...) {
     # cat("Euler:init \n")
-    object <- initSolver(object, stepSize)
-    # object@rate <- object@numEqn
-    cat("Euler:object@numEqn =", object@numEqn, "\n")
-    object@rate <- vector("numeric", object@numEqn)
-    cat("Euler:init:rate =", object@rate, "\n")
+    # object <- initSolver(object, stepSize)
+    object@stepSize <- stepSize
+    state <- getState(object@ode)
+    object@ode@state <- state
+    
+    if (is.null(state)) {
+        object@numEqn <-  0
+    } else {
+        # cat("assigning a value > 0 \n")
+        object@numEqn = length(state)
+        # cat("ODESOlver:object@numEqn =", object@numEqn, "\n")
+    }
+    object@ode@rate <- vector("numeric", object@numEqn)
     object
 })
 
@@ -52,6 +60,17 @@ setMethod("step", "Euler", function(object, ...) {
     object
 }) 
 
+setMethod("setStepSize", "Euler", function(object, stepSize, ...) {
+    object@stepSize = stepSize
+    # cat("step size is ", object@stepSize, "\n")
+    object
+})
+
+
+setMethod("getStepSize", "Euler", function(object, ...) {
+    return(object@stepSize)
+})
+
 
 # constructor
 Euler <- function(.ode) {
@@ -61,34 +80,3 @@ Euler <- function(.ode) {
     return(euler)
 }
 
-
-# setMethod("step", "Euler", function(object, ...) {
-#     # who calls `step()`? : it is called from the main application, from a loop
-#     cat(sprintf("%25s  %11s  %11s \n", "state_1", 
-#                 "state_2", "state_3"))
-#     
-#     object@ode@state <- getState(object@ode)
-#     object@ode@rate  <- getRate(object@ode, object@ode@state, object@rate)
-#     
-#     cat(sprintf("%12s %12f %12f %12f \n", "Euler:state", 
-#                 object@ode@state[1], object@ode@state[2], object@ode@state[3]))
-#     cat(sprintf("%12s %12f %12f %12f \n", "Euler:rate", 
-#                 object@ode@rate[1], object@ode@rate[2], object@ode@rate[3]))
-#     
-#     # cat("Euler:rate   =", rate, "\n")
-#     # cat("Euler:numEqn =", object@numEqn, "\n")
-# 
-#     cat(sprintf("%12s", "state(iter)"))
-#     
-#     for (i in 1:object@numEqn) {
-#         object@ode@state[i] <- object@ode@state[i] + object@stepSize * object@rate[i]
-#         
-#         # cat(sprintf("%13f", object@ode@state[i]))
-#         cat(sprintf("%13d", i))
-#     }
-#     cat("\n")
-#     # return(object@stepSize)
-#     # object@ode@state <- state
-#     # object@ode@rate  <- rate
-#     object
-# })    
