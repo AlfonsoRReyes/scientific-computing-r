64fb99046e470e7e0f77025cf83dae3b8467ee5d
alfonso zbook <zbook@reyesaguayo.com>
2017-05-01 16:46:35 -0500

    replace scripts with code that is working in FallingParticleApp.R

--- R/FallingParticleODE.R
+++ R/FallingParticleODE.R
@@ -3,24 +3,21 @@ source("./R/ODE.R")
 
 setClass("FallingParticleODE", slots = c(
     g = "numeric"
-    ), 
-    prototype = prototype(
-        g = 9.8
-    ),
+), 
+prototype = prototype(
+    g = 9.8
+),
 contains = c("ODE")
 )
 
 
 setMethod("initialize", "FallingParticleODE", function(.Object, ...) {
-    # .Object <- callNextMethod()
     .Object@state <- vector("numeric", 3)
     return(.Object)
 })
 
-
 setMethod("getState", "FallingParticleODE", function(object, ...) {
     # Gets the state variables.
-    # cat("getState() called with ", class(object), "\n")
     return(object@state)
 })
 
@@ -32,23 +29,11 @@ setMethod("getRate", "FallingParticleODE", function(object, state, rate, ...) {
     object@rate[3] <- 1
     object@rate   # last change
     
-    # rate[1] <-   state[2]
-    # rate[2] <- - object@g
-    # rate[3] <-   1
-    # 
-    # # object@rate <- rate
-    # # object@state <- state
-    # 
-    # return(rate)   # last change
-    
 })
-
-
 
 # constructor
 FallingParticleODE <- function(y, v) {
-    .FallingParticleODE <- new("FallingParticleODE", y, v)
-    # .FallingParticleODE@state <- vector("numeric", 3)
+    .FallingParticleODE <- new("FallingParticleODE")
     .FallingParticleODE@state[1] <- y
     .FallingParticleODE@state[2] <- v
     .FallingParticleODE@state[3] <- 0
