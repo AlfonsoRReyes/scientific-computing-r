f1f611dc12ab5bcfed6b6265b0e3d58a2e3db240
alfonso zbook <zbook@reyesaguayo.com>
2017-05-01 11:23:36 -0500

    remove the initialize part in ODE

--- R/ODE.R
+++ R/ODE.R
@@ -7,22 +7,22 @@ setClass("ODE", slots = c(
 ))
 
 
-setMethod("initialize", "ODE", function(.Object, ...) {
-    # cat("initializing class: ", class(.Object), "\n")
-    .Object@state <- vector("numeric")
-    .Object@rate  <- vector("numeric")
-    return(.Object)
-    # callNextMethod(.Object)
-})
+# setMethod("initialize", "ODE", function(.Object, ...) {
+#     # cat("initializing class: ", class(.Object), "\n")
+#     .Object@state <- vector("numeric")
+#     .Object@rate  <- vector("numeric")
+#     return(.Object)
+#     # callNextMethod(.Object)
+# })
 
 setMethod("getState", "ODE", function(object, ...) {
     # Gets the state variables.
     # cat("getState() called with ", class(object), "\n")
-    # return(object@state)
+    return(object@state)
 })
 
 setMethod("getRate", "ODE", function(object, state, rate, ...) {
     # Gets the rate of change using the argument's state variables.
     # cat("getRate()  called with ", class(object), "\n")
-    # return(object@rate)
+    return(object@rate)
 })
