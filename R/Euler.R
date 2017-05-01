7bddef1592df630ea95c39287a0f5b61cd20141d
alfonso zbook <zbook@reyesaguayo.com>
2017-05-01 16:42:07 -0500

    copy Euler ODE that runs to old script

--- R/Euler.R
+++ R/Euler.R
@@ -1,20 +1,15 @@
-# source("./R/ode_generics.R")
-# source("./R/ODESolver.R")
-source("./R/ODE.R")
+source("./R/ode_generics.R")
+source("./R/ODESolver.R")
+# source("./R/ODE.R")
 
 
-setClass("Euler", slots = c(
-    stepSize = "numeric",
-    numEqn   = "numeric",
-    ode      = "ODE"
-), prototype = prototype(
-    stepSize = 0.1,
-    numEqn = 0
-)
+setClass("Euler", slots = c( 
+    rate = "numeric" 
+),  
+contains = c("ODESolver") 
 )
 
 setMethod("initialize", "Euler", function(.Object, ode, ...) {
-    # .Object <- callNextMethod(.Object, ode)
     .Object@ode <- ode
     .Object@ode@rate <- vector("numeric")
     return(.Object)
@@ -22,48 +17,26 @@ setMethod("initialize", "Euler", function(.Object, ode, ...) {
 
 
 setMethod("init", "Euler", function(object, stepSize, ...) {
-    # cat("Euler:init \n")
-    # object <- initSolver(object, stepSize)
-    object@stepSize <- stepSize
-    state <- getState(object@ode)
-    object@ode@state <- state
-    
-    if (is.null(state)) {
-        object@numEqn <-  0
-    } else {
-        # cat("assigning a value > 0 \n")
-        object@numEqn = length(state)
-        # cat("ODESOlver:object@numEqn =", object@numEqn, "\n")
-    }
-    object@ode@rate <- vector("numeric", object@numEqn)
+    object <- callNextMethod(object, stepSize)         # call superclass init()
+    object@ode@rate <- vector("numeric", object@numEqn)  # make the rate vector
     object
 })
 
 
 setMethod("step", "Euler", function(object, ...) {
-    # who calls `step()`? : it is called from the main application, from a loop
     state <- getState(object@ode)
-    # cat("Euler:step:rate:", object@rate, "\n")
-    cat("Euler:step:state:", state, "\n")
-    # cat("object@stepSize:", object@stepSize, "\n")
-    # rate  <- getRate(object@ode, object@ode@state, object@rate)
-    
     rate  <- getRate(object@ode, state, object@rate)
-    cat("Euler:step:rate:", object@ode@rate, "\n")
     
     for (i in 1:object@numEqn) {
         state[i] <- state[i] + object@stepSize * rate[i]
-        # cat(i, state[i], rate[i], "\n")
     }
-    cat("Euler:step:state:", state, "\n")
-    object@ode@state <- state
-    object@ode@rate  <- rate  # does rate has to return?
+    object@ode@state <- state       # return state and rate for new iter
+    object@ode@rate  <- rate  
     object
 }) 
 
 setMethod("setStepSize", "Euler", function(object, stepSize, ...) {
     object@stepSize = stepSize
-    # cat("step size is ", object@stepSize, "\n")
     object
 })
 
@@ -74,10 +47,10 @@ setMethod("getStepSize", "Euler", function(object, ...) {
 })
 
 
+
 # constructor
 Euler <- function(.ode) {
     euler <- new("Euler", .ode)
-    # cat("euler@stepSize:", euler@stepSize, "\n")
     euler <- init(euler, euler@stepSize)
     return(euler)
 }
