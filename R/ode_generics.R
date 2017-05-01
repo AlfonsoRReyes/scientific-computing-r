f798d44411dd7bfe743a8532c2dcaf29b19adb2f
alfonso zbook <zbook@reyesaguayo.com>
2017-05-01 16:43:58 -0500

    replace old ode_generics script with coe that runs

--- R/ode_generics.R
+++ R/ode_generics.R
@@ -1,9 +1,6 @@
-
-# setGeneric("init", function(object, initState, ...) standardGeneric("init"))
-setGeneric("step", function(object, ...) standardGeneric("step"))
 setGeneric("getRate", function(object, state, rate, ...) standardGeneric("getRate"))
-setGeneric("setStepSize", function(object, stepSize, ...) standardGeneric("setStepSize"))
-setGeneric("getStepSize", function(object, time, ...) standardGeneric("getStepSize"))
-setGeneric("doStep", function(object, ...) standardGeneric("doStep"))
 setGeneric("getState", function(object, ...) standardGeneric("getState"))
 setGeneric("init", function(object, stepSize, ...) standardGeneric("init"))
+setGeneric("step", function(object, ...) standardGeneric("step"))
+setGeneric("setStepSize", function(object, stepSize, ...) standardGeneric("setStepSize"))
+setGeneric("getStepSize", function(object, ...) standardGeneric("getStepSize"))
