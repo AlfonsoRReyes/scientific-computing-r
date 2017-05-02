b08f86333da534cec0acc7e3f0eb2aed2e845d5c
alfonso zbook <zbook@reyesaguayo.com>
2017-05-02 07:44:27 -0500

    debugging: why numEqn is not updated and always zero

--- R/Planet.R
+++ R/Planet.R
@@ -10,43 +10,47 @@ setClass("Planet", slots = c(
 )
 
 setMethod("initialize", "Planet", function(.Object, ...) {
-    # cat("initializing class: ", class(.Object), "\n")
     .Object@GM <- 4 * sqrt(pi * pi)
     .Object@state <- vector("numeric", 5)
     ode <- new("ODE")
     .Object@odeSolver <- new("Euler", ode)
-    # return(.Object)
+    cat(".Object@odeSolver:numEqn", .Object@odeSolver@numEqn, "\n")
+    return(.Object)
     # callNextMethod(.Object, .Object@ode)
-    callNextMethod(.Object)
+    # callNextMethod(.Object)
 })
 
 setMethod("doStep", "Planet", function(object, ...) {
     # Gets the state variables.
     # cat("doStep() called with ", class(object), "\n")
     # object@odeSolver@stepSize <- step(object@odeSolver)
-    step(object@odeSolver)
+    object@odeSolver <- step(object@odeSolver)
     # cat(object@state[1], object@state[2])
     object
 })
 
 setMethod("init", "Planet", function(object, initState, ...) {
-    # cat("initState", initState, "\n")
+    cat("Planet:init:initState", initState, "\n")
     object@state <- initState
-    cat("state@init planet", object@state, "\n")
-    object@odeSolver <- initSolver(object@odeSolver, getStepSize(object@odeSolver))
+    cat("Planet:init:state =", object@state, "\n")
+    cat("Planet:init:len:state =", length(object@state), "\n")
+    # object <- callNextMethod(object, stepSize)        # call superclass init()
+    object@odeSolver <- init(object@odeSolver, getStepSize(object@odeSolver))
     object
 })
 
 setMethod("getRate", "Planet", function(object, state, rate, ...) {
     # Gets the rate of change using the argument's state variables.
-    cat("getRate()  called with ", class(object), "\n")
+    # cat("getRate()  called with ", class(object), "\n")
     r2 <- state[1] * state[1] + state[3] * state[3]
     r3 <- r2 * sqrt(r2)
     object@rate[1] <- state[2]
-    object@rate[2] <- state[1] / r3
+    object@rate[2] <- (- object@GM * state[1]) / r3
     object@rate[3] <- state[4]
-    object@rate[4] <- state[3] / r3
+    object@rate[4] <- (- object@GM * state[3]) / r3
     object@rate[5] <- 1
+    
+    object@state <- state
     object
 })
 
