0dc40a4738d5a074250549bde449c6a8a4510d77
alfonso zbook <zbook@reyesaguayo.com>
2017-05-02 13:33:40 -0500

    adding a 5 elements vector to rate. This is to check that it gets to the upper classes

--- R/Euler.R
+++ R/Euler.R
@@ -11,7 +11,8 @@ contains = c("ODESolver")
 setMethod("initialize", "Euler", function(.Object, ode, ...) {
     cat("Euler:initialize:.Object=", class(.Object), "\n")
     .Object@ode <- ode
-    .Object@ode@rate <- vector("numeric")
+    .Object@ode@rate <- vector("numeric", 5)
+    cat(".Object@ode@rate=", .Object@ode@rate, "\n")
     return(.Object)
     # callNextMethod(.Object, ode)
 })
