a35b348efbc7b8b8bead30f098c02f226c35e75e
alfonso zbook <zbook@reyesaguayo.com>
2017-05-03 10:07:59 -0500

    ProjectileApp finally working. It was necessary to copy the values from the inner ODE class and fields to the most exterior class Projectile.

--- R/ProjectileApp.R
+++ R/ProjectileApp.R
@@ -1,9 +1,16 @@
+# generic functions
 setGeneric("getRate", function(object, state, rate, ...) standardGeneric("getRate"))
 setGeneric("getState", function(object, ...) standardGeneric("getState"))
-setGeneric("init", function(object, stepSize, ...) standardGeneric("init"))
+# setGeneric("init", function(object, stepSize, ...) standardGeneric("init"))
 setGeneric("step", function(object, ...) standardGeneric("step"))
 setGeneric("setStepSize", function(object, stepSize, ...) standardGeneric("setStepSize"))
 setGeneric("getStepSize", function(object, ...) standardGeneric("getStepSize"))
+
+setGeneric("doStep", function(object, ...) standardGeneric("doStep"))
+setGeneric("setState", function(object, x, vx, y, vy, ...) standardGeneric("setState"))
+setGeneric("init", function(object, ...) standardGeneric("init"))
+
+####################################################### ODE -----------------
 
 setClass("ODE", slots = c(
     state = "numeric",
@@ -22,47 +29,9 @@ setMethod("getRate", "ODE", function(object, state, rate, ...) {
 })
 
 
-setClass("FallingParticleODE", slots = c(
-    g = "numeric"
-    ), 
-    prototype = prototype(
-        g = 9.8
-    ),
-    contains = c("ODE")
-    )
 
 
-setMethod("initialize", "FallingParticleODE", function(.Object, ...) {
-    .Object@state <- vector("numeric", 3)
-    return(.Object)
-})
-
-setMethod("getState", "FallingParticleODE", function(object, ...) {
-    # Gets the state variables.
-    return(object@state)
-})
-
-setMethod("getRate", "FallingParticleODE", function(object, state, rate, ...) {
-    # Gets the rate of change using the argument's state variables.
-    # cat("getRate()  called with ", class(object), "\n")
-    object@rate[1] <- state[2]
-    object@rate[2] <- - object@g
-    object@rate[3] <- 1
-    object@rate   # last change
-    
-})
-
-# constructor
-FallingParticleODE <- function(y, v) {
-    .FallingParticleODE <- new("FallingParticleODE")
-    .FallingParticleODE@state[1] <- y
-    .FallingParticleODE@state[2] <- v
-    .FallingParticleODE@state[3] <- 0
-    .FallingParticleODE
-}
-
-
-# ---------------------------------------- ODE Solver ----
+# -------------------------------------------------------------------- ODESolver
 
 setClass("ODESolver", slots = c(
     stepSize = "numeric",
@@ -74,12 +43,14 @@ setClass("ODESolver", slots = c(
 )
 )
 
-setMethod("initialize", "ODESolver", function(.Object, .ode, ...) {
+setMethod("initialize", "ODESolver", function(.Object, .ode) {
+    # cat("| 6 |")
+    .Object <- init(.Object, 0.1)
     return(.Object)
 })
 
 setMethod("step", "ODESolver", function(object, ...) {
-    object
+    # object                                            # change
 })
 
 setMethod("setStepSize", "ODESolver", function(object, stepSize, ...) {
@@ -88,8 +59,14 @@ setMethod("setStepSize", "ODESolver", function(object, stepSize, ...) {
 })
 
 setMethod("init", "ODESolver", function(object, stepSize, ...) {
+    # in this method we get the length of the state and assign it to numEqn
+    # cat("| 2 |")
+    # cat(object@ode@state)
     object@stepSize <- stepSize
+    # cat(object@stepSize)
     state <- getState(object@ode)
+    # cat("| 2.1 |", "\n")
+    # cat("ODESolver:init:state=", state, "\n")
     # object@ode@state <- state      # removing this does not have effect
     
     if (is.null(state)) {
@@ -109,17 +86,19 @@ setMethod("getStepSize", "ODESolver", function(object, ...) {
 ODESolver <- function(.ode) {
     odesolver <- new("ODESolver", .ode)
     odesolver@ode <- .ode
-    odesolver <- init(odesolver, 0.1)
+    # odesolver <- init(odesolver, 0.1)
     odesolver
 }
 
 
 
-# ----------------------------------------- Euler --------------------
 
-setClass("Euler", slots = c( 
-    rate = "numeric" 
-    ),  
+# ----------------------------------------------------------------- Euler ------
+
+setClass("Euler", 
+    #      slots = c( 
+    # rate = "numeric" 
+    # ),  
     contains = c("ODESolver") 
     )
 
@@ -138,10 +117,13 @@ setMethod("init", "Euler", function(object, stepSize, ...) {
 
 
 setMethod("step", "Euler", function(object, ...) {
-    state <- getState(object@ode)
-    rate  <- getRate(object@ode, state, object@rate)
+    # cat("| 4 |")
+    state <- getState(object@ode) # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    rate  <- getRate(object@ode, state, object@ode@rate)  # <<<<<<<<<<<<<<<<<<<<<<
     
-    for (i in 1:object@numEqn) {
+    # cat("Euler:step:state=", state, "rate:", rate, "numEqn", object@numEqn, "\n")
+    
+    for (i in 1:object@numEqn) {                         # numEqn not refreshing
         state[i] <- state[i] + object@stepSize * rate[i]
     }
     object@ode@state <- state       # return state and rate for new iter
@@ -156,7 +138,7 @@ setMethod("setStepSize", "Euler", function(object, stepSize, ...) {
 
 
 setMethod("getStepSize", "Euler", function(object, ...) {
-    object <- object
+    # object <- object
     return(object@stepSize)
 })
 
@@ -164,10 +146,94 @@ setMethod("getStepSize", "Euler", function(object, ...) {
 
 # constructor
 Euler <- function(.ode) {
+    # cat("| 1 |")
     euler <- new("Euler", .ode)
-    euler <- init(euler, euler@stepSize)
+    # euler <- new("ODESolver", .ode)
+    
+    # euler <- init(euler, euler@stepSize)
+    # euler <- init(ODESolver, euler@stepSize)                       # changed
+    
+    # euler <- ODESolver(.ode)
+
+    # cat("euler@numEqn=", euler@numEqn, "\n")
     return(euler)
 }
 
 
 
+
+
+# ------------------------------------------------------------------ Projectile
+
+setClass("Projectile", slots = c(
+    g = "numeric",
+    odeSolver = "Euler"
+    ),
+    prototype = prototype(
+        g = 9.8
+    ),
+    contains = c("ODE")
+    )
+
+setMethod("initialize", "Projectile", function(.Object) {
+    # .Object@odeSolver <- new("Euler", .Object)
+    # .Object@odeSolver <- Euler(.Object) <- gives error
+    # .Object@odeSolver <- init(.Object@odeSolver, .Object)
+    # .Object@odeSolver <- init(.Object@odeSolver, 0.1)   # no effect
+    # cat("| 0 |")
+    .Object@odeSolver <- Euler(.Object)
+    return(.Object)
+})
+
+setMethod("setStepSize", "Projectile", function(object, stepSize, ...) {
+    object@odeSolver <- setStepSize(object@odeSolver, stepSize)
+    object
+})
+
+
+setMethod("step", "Projectile", function(object) {
+    # cat("Projectile:step:state:rate(1)", object@state, object@rate, "\n")
+    # cat("| 3 |")
+    object@odeSolver <- step(object@odeSolver)
+    
+    object@rate  <- object@odeSolver@ode@rate
+    object@state <- object@odeSolver@ode@state
+    
+    # cat("Projectile:step:state:rate(2)", object@state, object@rate, "\n")
+    object
+})
+
+setMethod("setState", "Projectile", function(object, x, vx, y, vy) {
+    object@state[1] <- x
+    object@state[2] <- vx
+    object@state[3] <- y
+    object@state[4] <- vy
+    object@state[5] <- 0
+    
+    object@odeSolver@ode@state <- object@state
+    object
+})
+
+setMethod("getState", "Projectile", function(object) {
+    object@state
+})
+
+
+setMethod("getRate", "Projectile", function(object, state, rate) {
+    rate[1] <- state[2]
+    rate[2] <- 0
+    rate[3] <- state[4]
+    rate[4] <- - object@g
+    rate[5] <- 1
+    
+    object@state <- object@odeSolver@ode@state <- state
+    object@rate  <- object@odeSolver@ode@rate  <- rate
+    object@rate
+})
+
+
+
+
+
+# constructor
+Projectile <- function()  new("Projectile")
