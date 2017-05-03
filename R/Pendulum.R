3e39df9e1b8d257f48be1d078e6136a21f6eb525
alfonso zbook <zbook@reyesaguayo.com>
2017-05-03 13:02:41 -0500

    new class Pendulum

---
+++ R/Pendulum.R
@@ -0,0 +1,76 @@
+# Pendulum.R
+
+source("./R/ODE.R")
+source("./R/Euler.R")
+
+setClass("Pendulum", slots = c(
+    g = "numeric",
+    odeSolver = "Euler"
+    ),
+    prototype = prototype(
+        g = 9.8
+    ),
+    contains = c("ODE")
+    )
+
+setMethod("initialize", "Pendulum", function(.Object) {
+    # .Object@odeSolver <- new("Euler", .Object)
+    # .Object@odeSolver <- Euler(.Object) <- gives error
+    # .Object@odeSolver <- init(.Object@odeSolver, .Object)
+    # .Object@odeSolver <- init(.Object@odeSolver, 0.1)   # no effect
+    # cat("| 0 |")
+    .Object@odeSolver <- Euler(.Object)                               # diff 6
+    return(.Object)
+})
+
+setMethod("setStepSize", "Pendulum", function(object, stepSize, ...) {
+    object@odeSolver <- setStepSize(object@odeSolver, stepSize)
+    object
+})
+
+
+setMethod("step", "Pendulum", function(object) {
+    # cat("Projectile:step:state:rate(1)", object@state, object@rate, "\n")
+    object@odeSolver <- step(object@odeSolver)
+    
+    object@rate  <- object@odeSolver@ode@rate                           # diff 7
+    object@state <- object@odeSolver@ode@state                          # diff 8
+    
+    # cat("Projectile:step:state:rate(2)", object@state, object@rate, "\n")
+    object
+})
+
+setMethod("setState", "Pendulum", function(object, x, vx, y, vy) {
+    object@state[1] <- x
+    object@state[2] <- vx
+    object@state[3] <- y
+    object@state[4] <- vy
+    object@state[5] <- 0     # t + dt
+    
+    object@odeSolver@ode@state <- object@state
+    object
+})
+
+setMethod("getState", "Pendulum", function(object) {                 # diff 9
+    object@state
+})
+
+
+setMethod("getRate", "Pendulum", function(object, state, rate) {    # diff 10
+    rate[1] <- state[2]     # rate of change of x                                          # diff 11
+    rate[2] <- 0            # rate of change of vx                    # diff 12
+    rate[3] <- state[4]     # rate of change of y
+    rate[4] <- - object@g   # rate of change of vy
+    rate[5] <- 1            # dt/dt = 1
+    
+    object@state <- object@odeSolver@ode@state <- state
+    object@rate  <- object@odeSolver@ode@rate  <- rate
+    object@rate                                                       # diff 18
+})
+
+
+
+
+
+# constructor
+Pendulum <- function()  new("Pendulum")
