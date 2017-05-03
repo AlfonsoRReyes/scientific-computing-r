7c31e1abed39d1e7bc43c0508a078f9a693d91ce
alfonso zbook <zbook@reyesaguayo.com>
2017-05-03 10:08:54 -0500

    adding comments

--- R/Projectile.R
+++ R/Projectile.R
@@ -1,5 +1,5 @@
 # Projectile.R
-
+source("./R/ODE.R")
 source("./R/Euler.R")
 
 setClass("Projectile", slots = c(
@@ -15,11 +15,16 @@ setClass("Projectile", slots = c(
 )
 
 setMethod("initialize", "Projectile", function(.Object) {
-    .Object@odeSolver <- Euler(.Object)
-    .Object <- .Object@odeSolver@ode
-    cat(".Object@odeSolver@ode@rate=", .Object@odeSolver@ode@rate, "\n")
-    cat(".Object@rate=", .Object@rate, "\n")
-    cat(".Object@state=", .Object@state, "\n")
+    # .Object@odeSolver <- Euler(.Object)
+    # ode <- new("ODE")
+    # .Object@odeSolver <- new("Euler", ode)
+    .Object@odeSolver <- new("Euler", .Object)
+    # .Object@rate <- ode@rate
+    # .Object@state <- ode@state
+    # .Object <- .Object@odeSolver@ode
+    # cat(".Object@odeSolver@ode@rate=", .Object@odeSolver@ode@rate, "\n")
+    # cat(".Object@rate==", .Object@rate, "\n")
+    # cat(".Object@state=", .Object@state, "\n")
     # cat("Projectile:initialize:.Object@odeSolver=", class(.Object@odeSolver), "\n")
     # return(.Object)
     # ret <- callNextMethod()
@@ -33,12 +38,27 @@ setMethod("setStepSize", "Projectile", function(object, stepSize, ...) {
     object
 })
 
+
 setMethod("step", "Projectile", function(object) {
+    # cat("Projectile:step:state:rate(1)", object@state, object@rate, "\n")
     object@odeSolver <- step(object@odeSolver)
+    # cat("Projectile:step:state:rate(2)", object@state, object@rate, "\n")
     object
 })
 
 
+
+setMethod("setState", "Projectile", function(object, x, vx, y, vy) {
+    object@state[1] <- x
+    object@state[2] <- vx
+    object@state[3] <- y
+    object@state[4] <- vy
+    object@state[5] <- 0
+    object@odeSolver@ode@state <- object@state
+    object
+})
+
 # constructor
 
 Projectile <- function()  new("Projectile")
+
