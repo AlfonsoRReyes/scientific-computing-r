5c70189ee2abc95bbed4dd44f459d20de16bfe19
alfonso zbook <zbook@reyesaguayo.com>
2017-05-03 12:03:43 -0500

    new ODE class

---
+++ R/EulerRichardson.R
@@ -0,0 +1,52 @@
+source("./R/ODESolver.R")
+
+
+setClass("EulerRichardson", 
+    contains = c("ODESolver") 
+)
+
+setMethod("initialize", "EulerRichardson", function(.Object, ode, ...) {
+    .Object@ode <- ode
+    .Object@ode@rate <- vector("numeric")
+    return(.Object)
+})
+
+
+setMethod("init", "EulerRichardson", function(object, stepSize, ...) {
+    object <- callNextMethod(object, stepSize) # call superclass init  # diff 21
+    object@ode@rate <- vector("numeric", object@numEqn)  # make the rate vector
+    object
+})
+
+
+setMethod("step", "EulerRichardson", function(object, ...) {
+    state <- getState(object@ode)
+    rate  <- getRate(object@ode, state, object@ode@rate)             # diff 4
+    
+    for (i in 1:object@numEqn) {
+        state[i] <- state[i] + object@stepSize * rate[i]
+    }
+    object@ode@state <- state       # return state and rate for new iter
+    object@ode@rate  <- rate  
+    object
+}) 
+
+setMethod("setStepSize", "EulerRichardson", function(object, stepSize, ...) {
+    object@stepSize = stepSize
+    object
+})
+
+
+setMethod("getStepSize", "EulerRichardson", function(object, ...) {
+    return(object@stepSize)
+})
+
+
+
+# constructor
+EulerRichardson <- function(.ode) {
+    euler <- new("Euler", .ode)
+    euler <- init(euler, euler@stepSize)                         # diff 5
+    return(euler)
+}
+
