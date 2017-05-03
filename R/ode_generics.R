0395464a4f69ae74e9ff510499bd147627d9637d
alfonso zbook <zbook@reyesaguayo.com>
2017-05-03 10:33:30 -0500

    move init() to the bottom of ode_generics.R and keep only one parameter: "object".

--- R/ode_generics.R
+++ R/ode_generics.R
@@ -1,9 +1,11 @@
 setGeneric("getRate", function(object, state, rate, ...) standardGeneric("getRate"))
 setGeneric("getState", function(object, ...) standardGeneric("getState"))
-setGeneric("init", function(object, stepSize, ...) standardGeneric("init"))
+
 setGeneric("step", function(object, ...) standardGeneric("step"))
 setGeneric("setStepSize", function(object, stepSize, ...) standardGeneric("setStepSize"))
 setGeneric("getStepSize", function(object, ...) standardGeneric("getStepSize"))
 
 setGeneric("doStep", function(object, ...) standardGeneric("doStep"))
 setGeneric("setState", function(object, x, vx, y, vy, ...) standardGeneric("setState"))
+
+setGeneric("init", function(object, ...) standardGeneric("init"))
