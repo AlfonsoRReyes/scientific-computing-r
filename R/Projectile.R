8549250df18bb640b7ac736b68af1642e810f6e8
alfonso zbook <zbook@reyesaguayo.com>
2017-05-03 10:26:24 -0500

    replace Projectile.R with new class developed in ProjectileApp.R

--- R/Projectile.R
+++ R/Projectile.R
@@ -4,33 +4,22 @@ source("./R/Euler.R")
 
 setClass("Projectile", slots = c(
     g = "numeric",
-    # state = "numeric",
-    # test_1 = "numeric",
     odeSolver = "Euler"
     ),
     prototype = prototype(
         g = 9.8
     ),
     contains = c("ODE")
-)
+    )
 
 setMethod("initialize", "Projectile", function(.Object) {
-    # .Object@odeSolver <- Euler(.Object)
-    # ode <- new("ODE")
-    # .Object@odeSolver <- new("Euler", ode)
-    .Object@odeSolver <- new("Euler", .Object)
-    # .Object@rate <- ode@rate
-    # .Object@state <- ode@state
-    # .Object <- .Object@odeSolver@ode
-    # cat(".Object@odeSolver@ode@rate=", .Object@odeSolver@ode@rate, "\n")
-    # cat(".Object@rate==", .Object@rate, "\n")
-    # cat(".Object@state=", .Object@state, "\n")
-    # cat("Projectile:initialize:.Object@odeSolver=", class(.Object@odeSolver), "\n")
-    # return(.Object)
-    # ret <- callNextMethod()
-    # cat(class(ret))
+    # .Object@odeSolver <- new("Euler", .Object)
+    # .Object@odeSolver <- Euler(.Object) <- gives error
+    # .Object@odeSolver <- init(.Object@odeSolver, .Object)
+    # .Object@odeSolver <- init(.Object@odeSolver, 0.1)   # no effect
+    # cat("| 0 |")
+    .Object@odeSolver <- Euler(.Object)                               # diff 6
     return(.Object)
-    
 })
 
 setMethod("setStepSize", "Projectile", function(object, stepSize, ...) {
@@ -42,11 +31,13 @@ setMethod("setStepSize", "Projectile", function(object, stepSize, ...) {
 setMethod("step", "Projectile", function(object) {
     # cat("Projectile:step:state:rate(1)", object@state, object@rate, "\n")
     object@odeSolver <- step(object@odeSolver)
+    
+    object@rate  <- object@odeSolver@ode@rate                           # diff 7
+    object@state <- object@odeSolver@ode@state                          # diff 8
+    
     # cat("Projectile:step:state:rate(2)", object@state, object@rate, "\n")
     object
 })
-
-
 
 setMethod("setState", "Projectile", function(object, x, vx, y, vy) {
     object@state[1] <- x
@@ -54,11 +45,31 @@ setMethod("setState", "Projectile", function(object, x, vx, y, vy) {
     object@state[3] <- y
     object@state[4] <- vy
     object@state[5] <- 0
+    
     object@odeSolver@ode@state <- object@state
     object
 })
 
+setMethod("getState", "Projectile", function(object) {                 # diff 9
+    object@state
+})
+
+
+setMethod("getRate", "Projectile", function(object, state, rate) {    # diff 10
+    rate[1] <- state[2]                                               # diff 11
+    rate[2] <- 0                                                      # diff 12
+    rate[3] <- state[4]
+    rate[4] <- - object@g
+    rate[5] <- 1
+    
+    object@state <- object@odeSolver@ode@state <- state
+    object@rate  <- object@odeSolver@ode@rate  <- rate
+    object@rate                                                       # diff 18
+})
+
+
+
+
+
 # constructor
-
 Projectile <- function()  new("Projectile")
-
