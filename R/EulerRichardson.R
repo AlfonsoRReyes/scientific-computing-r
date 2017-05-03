6b12f60552d0504a98b0ba045c74ef4be0693da9
alfonso zbook <zbook@reyesaguayo.com>
2017-05-03 12:29:24 -0500

    add unit tests for EulerRichardson

--- R/EulerRichardson.R
+++ R/EulerRichardson.R
@@ -13,7 +13,9 @@ source("./R/ODESolver.R")
 #     * @author       Wolfgang Christian
 
 
-setClass("EulerRichardson", 
+setClass("EulerRichardson", slots = c(
+  midstate = "numeric"                         # this is the midpoint
+),
     contains = c("ODESolver") 
 )
 
@@ -25,19 +27,30 @@ setMethod("initialize", "EulerRichardson", function(.Object, ode, ...) {
 
 
 setMethod("init", "EulerRichardson", function(object, stepSize, ...) {
-    object <- callNextMethod(object, stepSize) # call superclass init  # diff 21
+    object <- callNextMethod(object, stepSize) # call superclass init
     object@ode@rate <- vector("numeric", object@numEqn)  # make the rate vector
+    object@midstate <- vector("numeric", object@numEqn)
     object
 })
 
 
 setMethod("step", "EulerRichardson", function(object, ...) {
     state <- getState(object@ode)
-    rate  <- getRate(object@ode, state, object@ode@rate)             # diff 4
+    rate  <- getRate(object@ode, state, object@ode@rate)            
+    
+    dt2 <- object@stepSize / 2
+    
+    for (i in 1:object@numEqn) {
+        # estimate the state a the midpoint
+        object@midstate[i] <- state[i] + rate[i] * dt2
+    }
+    
+    rate  <- getRate(object@ode, object@midstate, rate)
     
     for (i in 1:object@numEqn) {
         state[i] <- state[i] + object@stepSize * rate[i]
     }
+    
     object@ode@state <- state       # return state and rate for new iter
     object@ode@rate  <- rate  
     object
@@ -57,8 +70,8 @@ setMethod("getStepSize", "EulerRichardson", function(object, ...) {
 
 # constructor
 EulerRichardson <- function(.ode) {
-    euler <- new("Euler", .ode)
-    euler <- init(euler, euler@stepSize)                         # diff 5
-    return(euler)
+    eulerRichardson <- new("EulerRichardson", .ode)
+    eulerRichardson <- init(eulerRichardson, eulerRichardson@stepSize)                         # diff 5
+    return(eulerRichardson)
 }
 
