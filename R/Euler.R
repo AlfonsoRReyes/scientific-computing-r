1880e870412290a3459ca3dc6098abfa037bf771
alfonso zbook <zbook@reyesaguayo.com>
2017-05-03 15:38:02 -0500

    add author. remove diffs

--- R/Euler.R
+++ R/Euler.R
@@ -1,5 +1,15 @@
 source("./R/ODESolver.R")
 
+# /**
+# * Euler implements an Euler method ODE solver.
+# *
+# * The Euler method is unstable for many systems.  It is included as an  
+# * example of how to use the ODE and ODESolver interface.
+# *
+# * @author              Wolfgang Christian
+# * @version 1.0
+# * converted to R by    Alfonso R> Reyes
+# */
 
 setClass("Euler", 
     contains = c("ODESolver") 
@@ -13,7 +23,7 @@ setMethod("initialize", "Euler", function(.Object, ode, ...) {
 
 
 setMethod("init", "Euler", function(object, stepSize, ...) {
-    object <- callNextMethod(object, stepSize) # call superclass init  # diff 21
+    object <- callNextMethod(object, stepSize) # call superclass init  #
     object@ode@rate <- vector("numeric", object@numEqn)  # make the rate vector
     object
 })
@@ -21,7 +31,7 @@ setMethod("init", "Euler", function(object, stepSize, ...) {
 
 setMethod("step", "Euler", function(object, ...) {
     state <- getState(object@ode)
-    rate  <- getRate(object@ode, state, object@ode@rate)             # diff 4
+    rate  <- getRate(object@ode, state, object@ode@rate)             
     
     for (i in 1:object@numEqn) {
         state[i] <- state[i] + object@stepSize * rate[i]
@@ -46,7 +56,7 @@ setMethod("getStepSize", "Euler", function(object, ...) {
 # constructor
 Euler <- function(.ode) {
     euler <- new("Euler", .ode)
-    euler <- init(euler, euler@stepSize)                         # diff 5
+    euler <- init(euler, euler@stepSize)                         
     return(euler)
 }
 
