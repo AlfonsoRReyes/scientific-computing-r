0a6cc351d5f107b8dd5649d731b35e219d24a476
alfonso zbook <zbook@reyesaguayo.com>
2017-05-10 13:23:22 -0500

    new scripts for RK45

---
+++ R/ODEAdaptiveSolver.R
@@ -0,0 +1,25 @@
+source("./R/ode_generics.R")
+source("./R/ODESolver.R")
+
+
+setClass("ODEAdaptiveSolver", slots = c(
+    NO_ERROR = "numeric",
+    DID_NOT_CONVERGE = "numeric",
+    BISECTION_EVENT_NOT_FOUND = "numeric"
+    ), 
+    contains = c("ODESolver")
+)
+
+
+setMethod("setTolerance", "ODEAdaptiveSolver", function(object, tol) {
+    
+})
+
+
+setMethod("getTolerance", "ODEAdaptiveSolver", function(object) {
+    
+})
+
+setMethod("getErrorCode", "ODEAdaptiveSolver", function(object) {
+    
+})
---
+++ R/RK45.R
@@ -0,0 +1,23 @@
+source("./R/DormandPrince45.R")
+
+
+setClass("RK45", 
+    contains = c("DormandPrince45") 
+)
+
+setMethod("initialize", "RK45", function(.Object, ode, ...) {
+    # initialized the ODE solver
+    .Object@ode <- ode                          # set the ode to ODESolver slot
+    return(.Object)
+})
+
+
+
+
+# constructor ODE solver using Euler method
+RK45 <- function(.ode) {
+    # Euler constructor
+    rk45 <- new("RK45", .ode)                     # create the Euler object
+    return(rk45)
+}
+
