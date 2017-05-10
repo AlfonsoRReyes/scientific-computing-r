7f3d33854424a1fa1526ce6b75b9ba93dd5e9762
alfonso zbook <zbook@reyesaguayo.com>
2017-05-09 19:04:32 -0500

    very simple ODESolver

--- R/ODESolver.R
+++ R/ODESolver.R
@@ -1,21 +1,12 @@
 # ODESolver.R
 #
-source("./R/ODE.R")
 
 
-setClass("ODESolver", slots = c(
-    stepSize = "numeric",
-    numEqn   = "numeric",
-    ode      = "ODE"
-), prototype = prototype(
-    stepSize = 0.1,
-    numEqn = 0
-)
-)
+setClass("ODESolver")
 
-setMethod("initialize", "ODESolver", function(.Object, .ode, ...) {
-    .Object <- init(.Object, 0.1)                                 
-    return(.Object)
+
+setMethod("init", "ODESolver", function(object, stepSize, ...) {
+    object
 })
 
 setMethod("step", "ODESolver", function(object, ...) {
@@ -23,29 +14,9 @@ setMethod("step", "ODESolver", function(object, ...) {
 })
 
 setMethod("setStepSize", "ODESolver", function(object, stepSize, ...) {
-    object@stepSize = stepSize
-    object
-})
-
-setMethod("init", "ODESolver", function(object, stepSize, ...) {
-    object@stepSize <- stepSize
-    state <- getState(object@ode)
-    if (is.null(state)) {
-        object@numEqn <-  0
-    } else {
-        object@numEqn = length(state)
-    }
     object
 })
 
 setMethod("getStepSize", "ODESolver", function(object, ...) {
     return(object@stepSize)
 })
-
-
-# constructor
-ODESolver <- function(.ode) {
-    odesolver <- new("ODESolver", .ode)
-    odesolver@ode <- .ode
-    odesolver
-}
