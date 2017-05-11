a64a92cb3f51a5aa8c5743a55eb4b7542fd38383
alfonso zbook <zbook@reyesaguayo.com>
2017-05-11 11:00:07 -0500

    remove source of ode_generics.R

--- R/ODEAdaptiveSolver.R
+++ R/ODEAdaptiveSolver.R
@@ -1,15 +1,15 @@
-source("./R/ode_generics.R")
+# source("./R/ode_generics.R")
 source("./R/ODESolver.R")
 
 
 setClass("ODEAdaptiveSolver", slots = c(
-    NO_ERROR = "numeric",
-    DID_NOT_CONVERGE = "numeric",
+    NO_ERROR                  = "numeric",
+    DID_NOT_CONVERGE          = "numeric",
     BISECTION_EVENT_NOT_FOUND = "numeric"
     ), 
     prototype = prototype(
-        NO_ERROR = 0,
-        DID_NOT_CONVERGE = 1,
+        NO_ERROR                  = 0,
+        DID_NOT_CONVERGE          = 1,
         BISECTION_EVENT_NOT_FOUND = 2
     ),
     contains = c("ODESolver")
