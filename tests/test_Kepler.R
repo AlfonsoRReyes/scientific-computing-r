01c176042f0f14d6e02f3d6aa9f18b856ed322df
alfonso zbook <zbook@reyesaguayo.com>
2017-05-11 11:45:43 -0500

    add tests for matrix and arays

--- tests/test_Kepler.R
+++ tests/test_Kepler.R
@@ -35,16 +35,26 @@ expect_equal(getRate(planet, state, rate), c(0.00, -0.25,  0.25,  0.00,  1.00))
 })
 
 # now, we assign getRate to rate in object planet    
-planet@rate <- getRate(planet, state, rate)    
-planet@rate  
+# planet@rate <- getRate(planet, state, rate)    
+# planet@rate  
 
 solver <- RK45(planet)
-# solver <- DormandPrince45(planet)
 
-solver@a
-solver@b5
-solver@er
-solver@k
+expect_equal(solver@a , 
+             rbind( c(1.0/5.0, 0, 0, 0, 0), 
+               c(3.0/40.0, 9.0/40.0, 0, 0, 0), 
+               c(3.0/10.0, -9.0/10.0, 6.0/5.0, 0, 0), 
+               c(226.0/729.0, -25.0/27.0, 880.0/729.0, 55.0/729.0, 0), 
+               c(-181.0/270.0, 5.0/2.0, -266.0/297.0, -91.0/27.0, 189.0/55.0)))
+
+expect_equal(solver@b5, c(19.0/216.0, 0.0, 1000.0/2079.0, -125.0/216.0, 81.0/88.0, 5.0/56.0))
+expect_equal(solver@er, c(-11.0/360.0, 0.0, 10.0/63.0, -55.0/72.0, 27.0/40.0, -11.0/280.0))
+
+expect_equal(solver@numStages, 6)
+expect_equal(solver@numEqn, 5)
+
+expect_equal(solver@k, matrix(data = 0, nrow = solver@numStages, ncol = solver@numEqn))
+
 
 # print(planet)
 
