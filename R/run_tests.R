f6a717df9a79aee7240823ee75f6ec1762069aff
alfonso zbook <zbook@reyesaguayo.com>
2017-05-15 12:11:25 -0500

    add individual file tests

--- R/run_tests.R
+++ R/run_tests.R
@@ -6,12 +6,31 @@ source("./R/ODESolver.R")
 source("./R/AbstractODESolver.R")
 source("./R/Euler.R")
 source("./R/RK4.R")
+# source("./R/RK45.R")
 source("./R/EulerRichardson.R")
-source("./R/FallingParticleODE.R")
 
+source("./R/FallingParticleODE.R")
 source("./R/Pendulum.R")
 source("./R/Planet.R")
 source("./R/Projectile.R")
 source("./R/Reaction.R")
 
-test_dir("./tests", reporter = "Summary")
+
+
+# source("./R/Kepler.R")
+
+# test_file("./tests/test_FallingParticleODEApp.R")
+# test_file("./tests/test_Pendulum.R", reporter = "tap")
+# test_file("./tests/test_Planet.R", reporter = "tap")
+# test_file("./tests/test_Projectile.R", reporter = "tap")
+# test_file("./tests/test_ReactionApp.R", reporter = "summary")
+
+# test_file("./tests/test_AbstractODESolver.R", reporter = "summary")
+# test_file("./tests/test_Euler.R", reporter = "summary")
+# test_file("./tests/test_EulerRichardson.R", reporter = "summary")
+# test_file("./tests/test_ODESolver.R", reporter = "summary")
+# test_file("./tests/test_RK4.R", reporter = "summary")
+
+test_dir("./tests", reporter = "summary")
+
+
