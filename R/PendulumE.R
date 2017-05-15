ea7b54a014d3a6551890ba85f44ec9e93fd09624
alfonso zbook <zbook@reyesaguayo.com>
2017-05-15 12:09:07 -0500

    remove generic functions

--- R/PendulumE.R
+++ R/PendulumE.R
@@ -5,11 +5,6 @@ source("./R/ODE.R")
 source("./R/EulerRichardson.R")
 
 
-setGeneric("setState", function(object, theta, thetaDot, ...) 
-    standardGeneric("setState"))
-# setGeneric("setStepSize", function(object, dt, ...)
-#     standardGeneric("setStepSize"))
-
 
 setClass("Pendulum", slots = c(
     omega0Squared = "numeric",
